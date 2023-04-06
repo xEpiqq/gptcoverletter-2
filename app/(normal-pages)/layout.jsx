98f6960bd307799a0013719fdc4c22cefc575d7e
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2023-04-06 08:21:24 -0600

    metadata?

--- app/(normal-pages)/layout.jsx
+++ app/(normal-pages)/layout.jsx
@@ -5,6 +5,20 @@ import "node_modules/react-modal-video/css/modal-video.css";
 import "../../styles/index.css";
 import app from "../../components/FirebaseApp";
 
+export const metadata = {
+  title: "GPTCoverLetter",
+  description: "Generate cover letters for your job applications using GPT-3.5",
+
+  type: "website",
+  icons: {
+    icon: "/logo_blue.svg",
+    shortcut: "/logo_blue.svg",
+  },
+
+  // index the page
+  robots: "index, follow",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning lang="en">
