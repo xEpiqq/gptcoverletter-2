d8dc3b09d2a47c41d2fe7e301878958470d66780
Jayden Crowther <jaydencrowther@gmail.com>
2023-04-06 04:28:17 -0600

    about section two

--- components/About/AboutSectionTwo.jsx
+++ components/About/AboutSectionTwo.jsx
@@ -21,29 +21,26 @@ const AboutSectionTwo = () => {
             <div className="ml-24 wow fadeInUp max-w-[470px]" data-wow-delay=".2s">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Bug free code
+                  Intuitive feel
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                The dashboard is designed to be user friendly. No tutorials necessary, just plug, play, and generate as many cover letters as you please.
                 </p>
               </div>
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Premier support
+                  Complete customization
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt.
+                The interface was built with customization at the forefront of our minds. The creativity meter, resume uploader, and description box are in your hands.
                 </p>
               </div>
               <div className="mb-1">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Next.js
+                  Speed-centric design
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt
-                  consectetur adipiscing elit setim.
+                Every aspect of the design is in place to speed up your application process and land you your next job as fast as physically possible.
                 </p>
               </div>
             </div>
