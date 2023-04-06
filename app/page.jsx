f620145c7ec64f8a13a651961691353b5521ea2b
Jayden Crowther <jaydencrowther@gmail.com>
2023-04-06 03:47:18 -0600

    home changes

--- app/page.jsx
+++ app/page.jsx
@@ -17,13 +17,13 @@ export default function Home() {
     <>
       <Hero />
       <Features />
-      <Video />
+      {/* <Video /> */}
       <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
       <Testimonials />
-      <Pricing />
-      <Blog />
+      {/* <Pricing /> */}
+      {/* <Blog /> */}
       <Contact />
     </>
   );
